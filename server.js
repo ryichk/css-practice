580dd362eb95311516d77ccd53ef74cbebde0ea1
Ichiki Ryo <tohodaiichiki@gmail.com>
2021-01-11 15:52:14 +0900

    fix: index.html

--- server.js
+++ server.js
@@ -9,6 +9,14 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+app.get('/first-steps', function(req, res) {
+  res.sendFile(path.join(__dirname, 'first-steps.html'));
+});
+
+app.get('/building-blocks', function(req, res) {
+  res.sendFile(path.join(__dirname, 'building-blocks.html'));
+});
+
 app.listen(3000, function() {
   console.log('Web site listening on port 3000');
 });
