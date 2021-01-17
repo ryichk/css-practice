615acaa5f6cd5fe622733af1f84c77fcfade5d07
Ichiki Ryo <tohodaiichiki@gmail.com>
2021-01-17 22:38:53 +0900

    begin practice css layout

--- server.js
+++ server.js
@@ -17,6 +17,10 @@ app.get('/building-blocks', function(req, res) {
   res.sendFile(path.join(__dirname, 'building-blocks.html'));
 });
 
+app.get('/layout', function(req, res) {
+  res.sendFile(path.join(__dirname, 'layout.html'));
+});
+
 app.listen(3000, function() {
   console.log('Web site listening on port 3000');
 });
